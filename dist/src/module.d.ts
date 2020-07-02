a6e6f9bc4510581e328f393c685908241bd6e6a1
atumilovich <atumilovich@gmail.com>
2020-07-02 12:49:46 +0300

    angular 9.1.11

--- dist/src/module.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./component";
-import * as i2 from "./directive";
-export declare class AceEditorModule {
-    static ɵmod: i0.ɵɵNgModuleDefWithMeta<AceEditorModule, [typeof i1.AceEditorComponent, typeof i2.AceEditorDirective], never, [typeof i1.AceEditorComponent, typeof i2.AceEditorDirective]>;
-    static ɵinj: i0.ɵɵInjectorDef<AceEditorModule>;
-}
